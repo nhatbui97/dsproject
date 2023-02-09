bd7fa56cc3ab0c5c454c755fc2a0ab52caefcaf2
hungvipqt15 <91128905+hungvipqt15@users.noreply.github.com>
2023-02-09 10:29:16 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -8,12 +8,20 @@ import pandas as pd
 
 app = Flask(__name__)
 
+
 ridgemodel = pickle.load(open('ridgemodel.pkl','rb'))
 lassomodel = pickle.load(open('lassomodel.pkl','rb'))
 forestmodel = pickle.load(open('forestmodel.pkl','rb'))
 xgbmodel = pickle.load(open('xgbmodel.pkl','rb'))
-X_train_original = pd.read_csv('X_train_original.csv', index_col = 'Unnamed: 0')
+X_train_original = pd.read_csv('X_train_original.csv')
 cat_attribs = ['GPU_Memory_Type', 'OS', 'RAM_DDR']
+
+data_val = pd.read_csv("./train_val_test/test.csv", index_col = 'Unnamed: 0')
+data_val.drop(['Year', 'CPU_Default_Clock', 'CPU_Transistor_Size(nm)', 'GPU clock(MHz)',
+           'RAM_bus', 'CPU_Cores', 'GPU_Bus_Width(bit)', 'Display_Width(pixel)', 'Brand', 'CPU', 'GPU', 'Laptop_name'],
+           axis = 1, inplace = True)
+X_val = data_val.drop('Price(USD)', axis = 1)
+
 num_attribs = list(set(X_train_original.columns).difference(set(cat_attribs)))
 num_pipeline = Pipeline([
     ('std_scaler', StandardScaler())
@@ -25,6 +33,14 @@ full_pipeline = ColumnTransformer([
 ])
 full_pipeline.fit(X_train_original)
 
+
+ridgemodel = pickle.load(open('ridgemodel.pkl','rb'))
+input0 = pd.DataFrame([['Linux', 1, 1, 1, 1, 1, 'GDDR5', 1, 1, 3, 1, 1, 1, 1]], columns = X_val.columns)
+input0 = full_pipeline.transform(input0)
+print(ridgemodel.predict(input0))
+output=np.exp(ridgemodel.predict(input0)) 
+print(output)  
+
 @app.route('/')
 def home():
     return render_template('index.html')
@@ -33,20 +49,20 @@ def home():
 def predict():
     input_features = []
     input_features.append(request.form['OS'])
-    input_features.append(request.form['CPU_Threads'])
-    input_features.append(request.form['CPU_Boosted_Clock'])
-    input_features.append(request.form['CPU_L3'])
-    input_features.append(request.form['CPU_TDP'])
-    input_features.append(request.form['GPU_Memory'])
+    input_features.append(float(request.form['CPU_Threads']))
+    input_features.append(float(request.form['CPU_Boosted_Clock']))
+    input_features.append(float(request.form['CPU_L3']))
+    input_features.append(float(request.form['CPU_TDP']))
+    input_features.append(float(request.form['GPU_Memory']))
     input_features.append(request.form['GPU_Memory_Type'])
-    input_features.append(request.form['GPU_memory_clock'])
-    input_features.append(request.form['RAM'])
-    input_features.append(request.form['RAM_DDR'])
-    input_features.append(request.form['Storage'])
-    input_features.append(request.form['Display_Inch'])
-    input_features.append(request.form['Display_height'])
-    input_features.append(request.form['Weight'])
-    input_features = pd.Dataframe([input_features], columns = X_train_original.columns)
+    input_features.append(float(request.form['GPU_memory_clock']))
+    input_features.append(float(request.form['RAM']))
+    input_features.append(float(request.form['RAM_DDR']))
+    input_features.append(float(request.form['Storage']))
+    input_features.append(float(request.form['Display_Inch']))
+    input_features.append(float(request.form['Display_height']))
+    input_features.append(float(request.form['Weight']))
+    input_features = pd.DataFrame([input_features], columns = X_val.columns)
     input_features = full_pipeline.transform(input_features)
     if request.form['model'] == 'ridge':
         output=np.exp(ridgemodel.predict(input_features)) 
